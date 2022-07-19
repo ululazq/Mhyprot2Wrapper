6fd5f83aecae32c2eba906106ad97e627eed7e4d
ossul <ossul@DESKTOP-MA5JK10>
2022-07-19 12:53:12 +0700

    add input aspd mspd

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,6 +10,24 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            awal:
+            int aspd=0; 
+            float mspd=0;
+            Console.Write("ASPD : ");
+            aspd = int.Parse(Console.ReadLine());
+            Console.Write("MSPD : ");
+            mspd = float.Parse(Console.ReadLine());
+            Library library = new Library();
+            library.OpenProcess((uint)Process.GetProcessesByName("ProjectN-Win64-Shipping")[0].Id);
+            //Write to memory
+            library.Write<int>(aspd, "ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
+            library.Write<float>(mspd, "ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
+            //Read from memory
+            var current_attack_speed = library.Read<int>("ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
+            var current_movement_speed = library.Read<float>("ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
+            Console.WriteLine("Attackspeed : " + current_attack_speed);
+            Console.WriteLine("Movespeed : " + current_movement_speed);
+            library.CloseDriver();
             using (var eventHookFactory = new EventHookFactory())
             {
                 var keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
@@ -18,17 +36,17 @@ namespace Test
                 {
                     if (e.KeyData.Keyname.Equals("D0")&&e.KeyData.EventType.Equals(KeyEvent.down))
                     {
-                        Library library = new Library();
-                        library.OpenProcess((uint)Process.GetProcessesByName("ProjectN-Win64-Shipping")[0].Id);
+                        Library library2 = new Library();
+                        library2.OpenProcess((uint)Process.GetProcessesByName("ProjectN-Win64-Shipping")[0].Id);
                         //Write to memory
-                        library.Write<float>(720, "ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
-                        library.Write<int>(16000, "ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
+                        library2.Write<int>(aspd, "ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
+                        library2.Write<float>(mspd, "ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
                         //Read from memory
-                        var current_movement_speed = library.Read<float>("ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
-                        var current_attack_speed = library.Read<int>("ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
-                        Console.WriteLine("Movespeed : " + current_movement_speed);
-                        Console.WriteLine("Attackspeed : " + current_attack_speed);
-                        library.CloseDriver();
+                        var current_attack_speed2 = library2.Read<int>("ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
+                        var current_movement_speed2 = library2.Read<float>("ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
+                        Console.WriteLine("Attackspeed : " + current_attack_speed2);
+                        Console.WriteLine("Movespeed : " + current_movement_speed2);
+                        library2.CloseDriver();
                     }
                 };
                 //waiting here to keep this thread running           
