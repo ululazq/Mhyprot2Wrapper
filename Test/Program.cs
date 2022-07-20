4f570a8b85db844a0cae40c38b0542216269d36c
ossul <ossul@DESKTOP-MA5JK10>
2022-07-21 00:35:15 +0700

    new update 20220720

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
 using EventHook;
 using Mhyprot2Wrapper;
 
@@ -10,8 +9,20 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            var text = @"
+Max ASPD Recomendation
+Destroyer = 20500
+Witch = 19900
+Rogue = 16500
+Engineer = 16500
+Swordman = 20500
+
+Max MSPD Recomendation
+All Classes = 720
+";
             int aspd;
-            float mspd; 
+            float mspd;
+            Console.WriteLine(text);
             Console.Write("ASPD : ");
             aspd = int.Parse(Console.ReadLine());
             Console.Write("MSPD : ");
@@ -25,7 +36,11 @@ namespace Test
                 {
                     if (e.KeyData.Keyname.Equals("D0") && e.KeyData.EventType.Equals(KeyEvent.down))
                     {
-                        Hiddencp(aspd,mspd);
+                        Hiddencp(aspd, mspd);
+                    }
+                    else if(e.KeyData.Keyname.Equals("D9") && e.KeyData.EventType.Equals(KeyEvent.down))
+                    {
+                        Hiddencp(10000, 500);
                     }
                 };
                 //waiting here to keep this thread running           
@@ -40,11 +55,11 @@ namespace Test
             Library library = new Library();
             library.OpenProcess((uint)Process.GetProcessesByName("ProjectN-Win64-Shipping")[0].Id);
             //Write to memory
-            library.Write<int>(aspd, "ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
-            library.Write<float>(mspd, "ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
+            library.Write<int>(aspd, "ProjectN-Win64-Shipping.exe+063818E8,0,20,804");
+            library.Write<float>(mspd, "ProjectN-Win64-Shipping.exe+063818E8,0,A0,288,18C");
             //Read from memory
-            var current_attack_speed = library.Read<int>("ProjectN-Win64-Shipping.exe+0636F5C8,0,20,804");
-            var current_movement_speed = library.Read<float>("ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
+            var current_attack_speed = library.Read<int>("ProjectN-Win64-Shipping.exe+063818E8,0,20,804");
+            var current_movement_speed = library.Read<float>("ProjectN-Win64-Shipping.exe+063818E8,0,A0,288,18C");
             Console.WriteLine("Attackspeed : " + current_attack_speed);
             Console.WriteLine("Movespeed : " + current_movement_speed);
             library.CloseDriver();
