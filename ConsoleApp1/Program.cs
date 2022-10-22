20a405ae330a0ea1957abf9a0a673bcb50d34821
ulul ulul <ossulul@gmail.com>
2022-10-22 22:40:10 +0700

    update new gworld

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using EventHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var eventHookFactory = new EventHookFactory();
+
+            var keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
+            keyboardWatcher.Start();
+            keyboardWatcher.OnKeyInput += (s, e) =>
+            {
+                Console.WriteLine("Key {0} event of key {1}", e.KeyData.EventType, e.KeyData.Keyname);
+            };
+
+            var mouseWatcher = eventHookFactory.GetMouseWatcher();
+            mouseWatcher.Start();
+            mouseWatcher.OnMouseInput += (s, e) =>
+            {
+                Console.WriteLine("Mouse event {0} at point {1},{2}", e.Message.ToString(), e.Point.x, e.Point.y);
+            };
+
+            var clipboardWatcher = eventHookFactory.GetClipboardWatcher();
+            clipboardWatcher.Start();
+            clipboardWatcher.OnClipboardModified += (s, e) =>
+            {
+                Console.WriteLine("Clipboard updated with data '{0}' of format {1}", e.Data,
+                    e.DataFormat.ToString());
+            };
+
+
+            var applicationWatcher = eventHookFactory.GetApplicationWatcher();
+            applicationWatcher.Start();
+            applicationWatcher.OnApplicationWindowChange += (s, e) =>
+            {
+                Console.WriteLine("Application window of '{0}' with the title '{1}' was {2}",
+                    e.ApplicationData.AppName, e.ApplicationData.AppTitle, e.Event);
+            };
+
+            var printWatcher = eventHookFactory.GetPrintWatcher();
+            printWatcher.Start();
+            printWatcher.OnPrintEvent += (s, e) =>
+            {
+                Console.WriteLine("Printer '{0}' currently printing {1} pages.", e.EventData.PrinterName,
+                    e.EventData.Pages);
+            };
+
+
+            Console.Read();
+
+            keyboardWatcher.Stop();
+            mouseWatcher.Stop();
+            clipboardWatcher.Stop();
+            applicationWatcher.Stop();
+            printWatcher.Stop();
+
+            eventHookFactory.Dispose();
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -8,26 +8,11 @@ namespace Test
     class Program
     {
         static void Main(string[] args)
-        {
-            var text = @"
-Max ASPD Recomendation
-Destroyer = 20500
-Witch = 19900
-Rogue = 16500
-Engineer = 16500
-Swordman = 20500
-
-Max MSPD Recomendation
-All Classes = 720
-";
-            int aspd;
-            float mspd;
-            Console.WriteLine(text);
-            Console.Write("ASPD : ");
-            aspd = int.Parse(Console.ReadLine());
-            Console.Write("MSPD : ");
-            mspd = float.Parse(Console.ReadLine());
-            Hiddencp(aspd, mspd);
+        {            
+            float td;
+            Console.Write("TD : ");
+            td = float.Parse(Console.ReadLine());
+            Hiddencp(td);
             using (var eventHookFactory = new EventHookFactory())
             {
                 var keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
@@ -36,32 +21,44 @@ All Classes = 720
                 {
                     if (e.KeyData.Keyname.Equals("D0") && e.KeyData.EventType.Equals(KeyEvent.down))
                     {
-                        Hiddencp(aspd, mspd);
+                        Hiddencp(td);
                     }
                     else if(e.KeyData.Keyname.Equals("D9") && e.KeyData.EventType.Equals(KeyEvent.down))
                     {
-                        Hiddencp(10000, 500);
+                        Hiddencp(1);
                     }
+                };
+                var mouseWatcher = eventHookFactory.GetMouseWatcher();
+                mouseWatcher.Start();
+                mouseWatcher.OnMouseInput += (s, e) =>
+                {
+                    if (e.Message.ToString().Equals("WM_XBUTTONDOWN"))
+                    {
+                        Hiddencp(td);
+                    }
+                    else if (e.Message.ToString().Equals("WM_RBUTTONDOWN"))
+                    {
+                        Hiddencp(1);
+                    }
+                    //Console.WriteLine(string.Format("Mouse event {0} at point {1},{2}", e.Message.ToString(), e.Point.x, e.Point.y));
                 };
                 //waiting here to keep this thread running           
                 Console.Read();
                 //stop watching
                 keyboardWatcher.Stop();
+                mouseWatcher.Stop();
             }
         }
 
-        static void Hiddencp(int aspd,float mspd)
+        static void Hiddencp(float td)
         {
             Library library = new Library();
             library.OpenProcess((uint)Process.GetProcessesByName("ProjectN-Win64-Shipping")[0].Id);
             //Write to memory
-            library.Write<int>(aspd, "ProjectN-Win64-Shipping.exe+063818E8,0,20,804");
-            library.Write<float>(mspd, "ProjectN-Win64-Shipping.exe+063818E8,0,A0,288,18C");
+            library.Write<float>(td,"ProjectN-Win64-Shipping.exe+0664C8E8,0,20,98");
             //Read from memory
-            var current_attack_speed = library.Read<int>("ProjectN-Win64-Shipping.exe+063818E8,0,20,804");
-            var current_movement_speed = library.Read<float>("ProjectN-Win64-Shipping.exe+063818E8,0,A0,288,18C");
-            Console.WriteLine("Attackspeed : " + current_attack_speed);
-            Console.WriteLine("Movespeed : " + current_movement_speed);
+            var current_td = library.Read<float>("ProjectN-Win64-Shipping.exe+0664C8E8,0,20,98");
+            Console.WriteLine("TD : " + current_td);
             library.CloseDriver();
         }
     }
