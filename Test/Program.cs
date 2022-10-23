af967fbfa6ca8c4fd61ad5a22e9cc89b0b66b044
ulul ulul <ossulul@gmail.com>
2022-10-23 14:35:58 +0700

    add unlimited jump

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,7 +12,7 @@ namespace Test
             float td;
             Console.Write("TD : ");
             td = float.Parse(Console.ReadLine());
-            Hiddencp(td);
+            Hiddencp(td,100);
             using (var eventHookFactory = new EventHookFactory())
             {
                 var keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
@@ -21,11 +21,11 @@ namespace Test
                 {
                     if (e.KeyData.Keyname.Equals("D0") && e.KeyData.EventType.Equals(KeyEvent.down))
                     {
-                        Hiddencp(td);
+                        Hiddencp(td,100);
                     }
                     else if(e.KeyData.Keyname.Equals("D9") && e.KeyData.EventType.Equals(KeyEvent.down))
                     {
-                        Hiddencp(1);
+                        Hiddencp(1,1);
                     }
                 };
                 var mouseWatcher = eventHookFactory.GetMouseWatcher();
@@ -34,11 +34,11 @@ namespace Test
                 {
                     if (e.Message.ToString().Equals("WM_XBUTTONDOWN"))
                     {
-                        Hiddencp(td);
+                        Hiddencp(td,100);
                     }
                     else if (e.Message.ToString().Equals("WM_RBUTTONDOWN"))
                     {
-                        Hiddencp(1);
+                        Hiddencp(1,1);
                     }
                     //Console.WriteLine(string.Format("Mouse event {0} at point {1},{2}", e.Message.ToString(), e.Point.x, e.Point.y));
                 };
@@ -50,15 +50,18 @@ namespace Test
             }
         }
 
-        static void Hiddencp(float td)
+        static void Hiddencp(float td,int jump)
         {
             Library library = new Library();
             library.OpenProcess((uint)Process.GetProcessesByName("ProjectN-Win64-Shipping")[0].Id);
             //Write to memory
             library.Write<float>(td,"ProjectN-Win64-Shipping.exe+0664C8E8,0,20,98");
+            library.Write<int>(jump, "ProjectN-Win64-Shipping.exe+0664C8E8,0,20,344");
             //Read from memory
             var current_td = library.Read<float>("ProjectN-Win64-Shipping.exe+0664C8E8,0,20,98");
+            var current_jump = library.Read<int>("ProjectN-Win64-Shipping.exe+0664C8E8,0,20,344");
             Console.WriteLine("TD : " + current_td);
+            Console.WriteLine("Jump : " + current_jump);
             library.CloseDriver();
         }
     }
